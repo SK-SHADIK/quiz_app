b4fee2a6605eaeda5329d7bdb0a0117a81991ecc
sk shadik <57946665+SK-SHADIK@users.noreply.github.com>
2023-09-23 23:12:06 +0600

    Edit Delete View In Question Answer

--- resources/views/Admin/Layouts/navbar.blade.php
+++ resources/views/Admin/Layouts/navbar.blade.php
@@ -10,12 +10,12 @@
     <body>
         <nav class="navbar">
             <ul class="nav-list">
-                <li><a href="adminDashboard.html">Home</a></li>
-                <li><a href="adminMarks.html">Marks</a></li>
-                <li><a href="adminReadingContent.html">Reading Content</a></li>
-                <li><a href="adminQuestions.html">Questions</a></li>
-                <li><a href="adminUser.html">Users</a></li>
-                <li><a href="adminProfile.html"><img src="{{ asset('img/profilePic.png') }}" alt=""></a></li>
+                <li><a href="">Home</a></li>
+                <li><a href="">Marks</a></li>
+                <li><a href="">Reading Content</a></li>
+                <li><a href="{{ route('show.question.answer') }}">Question Answer</a></li>
+                <li><a href="">Users List</a></li>
+                <li><a href=""><img src="{{ asset('img/profilePic.png') }}" alt=""></a></li>
             </ul>
         </nav>
 
